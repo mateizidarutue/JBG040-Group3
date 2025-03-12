58d20d944b03595c9ab3e2a939604b26cb108696
Tudor Vilceanu <t.vilceanu@student.tue.nl>
2025-03-12 22:27:55 +0100

    Model architecture changes

--- dc1/net.py
+++ dc1/net.py
@@ -1,42 +1,46 @@
 import torch
 import torch.nn as nn
 
-
 class Net(nn.Module):
-    def __init__(self, n_classes: int) -> None:
+    def __init__(self, n_classes: int = 6) -> None:
         super(Net, self).__init__()
 
         self.cnn_layers = nn.Sequential(
-            # Defining a 2D convolution layer
-            nn.Conv2d(1, 64, kernel_size=4, stride=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=4),
-            torch.nn.Dropout(p=0.5, inplace=True),
-            # Defining another 2D convolution layer
-            nn.Conv2d(64, 32, kernel_size=4, stride=1),
+            #Convolution Block 1
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3),
-            torch.nn.Dropout(p=0.25, inplace=True),
-            # Defining another 2D convolution layer
-            nn.Conv2d(32, 16, kernel_size=4, stride=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-            torch.nn.Dropout(p=0.125, inplace=True),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            #Convolution Block 2
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, groups=32),  # Depthwise Conv
+            nn.Conv2d(64, 64, kernel_size=1),  # Pointwise Conv (Reduces parameters)
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            #Convolution Block 3
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, groups=64),  # Depthwise Conv
+            nn.Conv2d(128, 128, kernel_size=1),  # Pointwise Conv
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.AdaptiveAvgPool2d((4, 4))
+
         )
 
-        self.linear_layers = nn.Sequential(
-            nn.Linear(144, 256),
-            nn.Linear(256, n_classes)
+        self.fc_layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128*4*4, 64),  # Reduced FC layer size
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.3),
+            nn.Linear(64, n_classes)  #Small FC layer
+
+
         )
 
-    # Defining the forward pass
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.cnn_layers(x)
-        # After our convolutional layers which are 2D, we need to flatten our
-        # input to be 1 dimensional, as the linear layers require this.
-        x = x.view(x.size(0), -1)
-        x = self.linear_layers(x)
+        x = self.fc_layers(x)
         return x
