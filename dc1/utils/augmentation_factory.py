5db405c76599cfcc9e2c7c62ee6a1dd04db370c2
JanProfesor <janprofesor@gmail.com>
2025-03-17 11:23:12 +0100

    fix(augmentation): change get to direct access

--- dc1/utils/augmentation_factory.py
+++ dc1/utils/augmentation_factory.py
@@ -5,23 +5,22 @@ class AugmentationFactory:
     def get_augmentations(config):
         augmentations = []
 
-        if config.get("use_flip"):
-            flip_prob = config.get("flip_prob")
+        if config["use_flip"]:
+            flip_prob = config["flip_prob"]
             augmentations.append(transforms.RandomHorizontalFlip(p=flip_prob))
 
-        if config.get("use_crop"):
-            crop_size = config.get("crop_size")
+        if config["use_crop"]:
+            crop_size = config["crop_size"]
             augmentations.append(transforms.RandomResizedCrop(size=crop_size))
 
-        if config.get("use_brightness") or config.get("use_contrast"):
-            brightness = config.get("brightness") if config.get("use_brightness") else 0
-            contrast = config.get("contrast") if config.get("use_contrast") else 0
+        if config["use_brightness"] or config["use_contrast"]:
+            brightness = config["brightness"] if config["use_brightness"] else 0
+            contrast = config["contrast"] if config["use_contrast"] else 0
             augmentations.append(transforms.ColorJitter(brightness=brightness, contrast=contrast))
 
-
-        if config.get("use_upscale"):
-            upscale = config.get("upscale")
-            upscale_mode = config.get("upscale_mode", "bilinear")
+        if config["use_upscale"]:
+            upscale = config["upscale"]
+            upscale_mode = config["upscale_mode"]
             augmentations.append(
                 transforms.Resize(
                     size=(int(upscale * 100), int(upscale * 100)), 
